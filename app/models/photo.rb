529d976d3becb27efdd4491d5da8d01167a3246a
Andre Cesar Gomes <andre270492@gmail.com>
2019-11-20 14:19:27 -0200

    add validation for photos form, and minor css changes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,14 @@
 class Photo < ApplicationRecord
   has_one_attached :picture
+  validate :correct_image_type
+
+  private
+
+  def correct_image_type
+    if picture.attached? && !picture.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:picture, 'A imagem precisa ser JPG ou PNG.')
+    elsif picture.attached? == false
+      errors.add(:picture, 'Uma imagem é necessária.')      
+    end
+  end
 end
