e131fbd96b8080de118e06a776193f3e21d18beb
Andre Cesar Gomes <andre270492@gmail.com>
2019-11-22 14:42:08 -0200

    faster animations and removed turbolinks for index page

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,34 +1,25 @@
-// slide up animation
-// 'n' and 'i' depend on the number of items "per page" on the controller
-var n = 0
-function imageMoveUp() {  
-  // if ($('.image-box').eq([n+i]) == undefined) {return} // prevent errors in console
-  console.log(n);
-  // $('.image-box').eq([n]).animate({top: '-60px'});
-  $('.image-box').eq(n).fadeTo(1000, 0.6);
-  n++;
-};
+$(document).ready(function(){
+  // only run for 'photos#index'
+  if ($('meta[name=psj]').attr('controller') == 'photos' && $('meta[name=psj]').attr('action') == 'index') 
+  {    
+    // slide up animation
+    var n = 0
+    function imageFadeIn() {  
+      $('.image-box').eq(n).fadeTo(600, 0.6);
+      n++;
+    };
 
-// infinite scroll stuff
-var loadNextPage = setInterval(function() {
-  if ($('#next_link').data('loading')){ return }  // prevent multiple loading
-  var wBottom  = $(window).scrollTop() + $(window).height() + 170;
-  var elBottom = $('#diamond-grid').offset().top + $('#diamond-grid').height();
-    // console.log(wBottom, elBottom)
-    if (wBottom > elBottom){
-      // console.log('rodando dentro')
-      if ($('#next_link')[0] == undefined) {clearInterval(loadNextPage); return} // reset the interval
-      $('#next_link')[0].click();
-      setTimeout(imageMoveUp, 100)
-      $('#next_link').data('loading', true);    
-    }
-  }, 300);
-
-
-// window.addEventListener('resize', loadNextPage);
-// window.addEventListener('scroll', loadNextPage);
-// window.addEventListener('load',  loadNextPage);
-// window.addEventListener('load',   imageMoveUp);
-// if($('.image-box').eq(0).complete) { console.log('entrouuu') };
-
-// $( window ).on("load", imageMoveUp);
+    // infinite scroll stuff
+    var loadNextPage = setInterval(function() {
+      if ($('#next_link').data('loading')){ return }  // prevent multiple loading
+      var wBottom  = $(window).scrollTop() + $(window).height() + 170;
+      var elBottom = $('#diamond-grid').offset().top + $('#diamond-grid').height();
+        if (wBottom > elBottom){
+          setTimeout(imageFadeIn, 50)
+          if ($('#next_link')[0] == undefined) {clearInterval(loadNextPage); return} // reset the interval
+          $('#next_link')[0].click();
+          $('#next_link').data('loading', true);    
+        }
+      }, 200);
+  }
+})
