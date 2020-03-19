a996b9ff2df2ea9cad6bde9b0840b7127349b458
Andre Cesar Gomes <andre270492@gmail.com>
2020-03-19 10:21:51 -0300

    Removed route for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'photos#index'
   resources :photos do
     collection do
-      delete 'destroy_multiple'
-      get 'admin_index'         #pesquisar o lugar apropriado
+      # delete 'destroy_multiple'
+      # get 'admin_index'         #pesquisar o lugar apropriado
     end
   end
 end
